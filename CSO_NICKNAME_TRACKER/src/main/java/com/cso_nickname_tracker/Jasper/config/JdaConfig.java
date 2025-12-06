ce2d37b848946de28d8fcfd5a44ff561692ed111
willard9909 <willard9909@gmail.com>
2025-12-06 12:29:32 +0900

    전역 명령어 관련 버그 수정

--- CSO_NICKNAME_TRACKER/src/main/java/com/cso_nickname_tracker/Jasper/config/JdaConfig.java
+++ CSO_NICKNAME_TRACKER/src/main/java/com/cso_nickname_tracker/Jasper/config/JdaConfig.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
